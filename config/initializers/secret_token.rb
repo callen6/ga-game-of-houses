f3d426ee653f4d8aeb3c372d8da024561cdcfff5
Cori Allen <coriallen@gmail.com>
2013-10-16 13:44:29 -0400

    Creates rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GameOfHouses::Application.config.secret_key_base = 'c9bd05d380d60dcf9993bf8313c98609fb02e86057ebd2c5b126f230295a2a33d89f7abc04150c7867e4b326333624bb45cf58d656be49aa660816772dc715cb'
